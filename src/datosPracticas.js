d50d447736b2247fd1bfd461924202c377acfce5
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-04-29 20:57:56 -0700

    Se agrega a la funcion el poder enviar el nombre, descripcion, fecha y enlace, para retornar esos valores

--- src/datosPracticas.js
+++ src/datosPracticas.js
@@ -3,12 +3,14 @@ class Practicas{
     this.nombre = null;
     this.descripcion = null;
     this.fecha = null;
+    this.enlace = null;
   }
 
-  cargarDatos(nombre, descripcion, fecha){
+  cargarDatos(nombre, descripcion, fecha, enlace){
     this.nombre = nombre;
     this.descripcion = descripcion;
     this.fecha = fecha;
+    this.enlace = enlace;
   }
 }
 
