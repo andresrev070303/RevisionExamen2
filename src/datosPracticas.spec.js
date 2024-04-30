706e61708fc71090933abb2f2891bc5f2ac9f0c6
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-04-29 21:00:06 -0700

    Refactorizacion e implementacion de interfaz para mostrar y agregar datos de las practicas

--- src/datosPracticas.spec.js
+++ src/datosPracticas.spec.js
@@ -16,4 +16,9 @@ describe("Crear un programa gamificado para TDDLab", () => {
       practica.cargarDatos("FizzBuzz", "Una practica de TDD donde se retorna una cadena de Fizz, Buzz o FizzBuzz de acuerdo a ciertas reglas","20/02/2024");
       expect(practica.fecha).toEqual("20/02/2024");
   });
+  it("Si ingreso el nombre, la descripcion, la fecha y el enlace de una practica, me regresa el nombre, la descripcion, la fecha y el enlace en una clase", () => {
+    const practica = new Practicas();
+    practica.cargarDatos("FizzBuzz", "Una practica de TDD donde se retorna una cadena de Fizz, Buzz o FizzBuzz de acuerdo a ciertas reglas","20/02/2024", "https://github.com/DeimaxRow1342/SecuenciaFizzBuzz");
+    expect(practica.enlace).toEqual("https://github.com/DeimaxRow1342/SecuenciaFizzBuzz");
+});
 });
--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,57 @@
-import sumar from "./sumador";
+import Practicas from "./datosPracticas.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+let nombres = ["FizzBuzz", "Totalizador"];
+let descripciones = ["Una practica de TDD donde se retorna una cadena de Fizz, Buzz o FizzBuzz de acuerdo a ciertas reglas", "Una practica de TDD donde se realiza un totalizador que calcula el precio total de una cantidad de productos aplicando ciertos impuestos y descuentos"];
+let fechas = ["2024-02-20", "2024-03-24"];
+let enlaces = ["https://github.com/DeimaxRow1342/SecuenciaFizzBuzz", "https://github.com/Dylancalle/Tarea7"];
+let practicas = [];
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+document.addEventListener("DOMContentLoaded", function() {
+  cargarPracticasIniciales();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  document.getElementById("proyectoForm").addEventListener("submit", function(event) {
+    event.preventDefault();
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+    const nombre = document.getElementById("nombre").value.toString();
+    const descripcion = document.getElementById("descripcion").value.toString();
+    const fecha = document.getElementById("fecha").value.toString();
+    const enlace = document.getElementById("enlace").value.toString();
+
+    const nuevaPractica = new Practicas();
+    nuevaPractica.cargarDatos(nombre, descripcion, fecha, enlace);
+    practicas.push(nuevaPractica);
+
+    this.reset();
+
+    actualizarTablaPracticas();
+  });
 });
+
+function cargarPracticasIniciales() {
+  for (let i = 0; i < nombres.length; i++) {
+    const nuevaPractica = new Practicas();
+    nuevaPractica.cargarDatos(nombres[i], descripciones[i], fechas[i], enlaces[i]);
+    practicas.push(nuevaPractica);
+  }
+
+  actualizarTablaPracticas();
+}
+
+function actualizarTablaPracticas() {
+  const contenido = document.querySelector("#tabla-practicas");
+  let salida = "";
+
+  practicas.forEach(practica => {
+    salida += `
+      <tr>
+        <td>${practica.nombre}</td>
+        <td>${practica.descripcion}</td>
+        <td>${practica.fecha}</td>
+        <td><a href="${practica.enlace}">${practica.enlace}</a></td>
+        <td><button onclick="eliminarPractica('${practica.nombre}')">Eliminar</button></td>
+      </tr>
+    `;
+  });
+
+  contenido.innerHTML = salida;
+}
