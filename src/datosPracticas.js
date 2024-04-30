1e1540aec8eda608941919af8785bce97f918034
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-04-29 20:56:44 -0700

    Se agrega a la funcion de la clase el poder agregar la fecha, y retornarlo junto con los demas valores

--- src/datosPracticas.js
+++ src/datosPracticas.js
@@ -2,11 +2,13 @@ class Practicas{
   constructor(){
     this.nombre = null;
     this.descripcion = null;
+    this.fecha = null;
   }
 
-  cargarDatos(nombre, descripcion){
+  cargarDatos(nombre, descripcion, fecha){
     this.nombre = nombre;
     this.descripcion = descripcion;
+    this.fecha = fecha;
   }
 }
 
--- src/datosPracticas.spec.js
+++ src/datosPracticas.spec.js
@@ -11,4 +11,9 @@ describe("Crear un programa gamificado para TDDLab", () => {
     practica.cargarDatos("FizzBuzz", "Una practica de TDD donde se retorna una cadena de Fizz, Buzz o FizzBuzz de acuerdo a ciertas reglas");
     expect(practica.descripcion).toEqual("Una practica de TDD donde se retorna una cadena de Fizz, Buzz o FizzBuzz de acuerdo a ciertas reglas");
   });
+  it("Si ingreso el nombre, la descripcion y la fecha de una practica, me regresa el nombre, la descripcion y la fecha en una clase", () => {
+      const practica = new Practicas();
+      practica.cargarDatos("FizzBuzz", "Una practica de TDD donde se retorna una cadena de Fizz, Buzz o FizzBuzz de acuerdo a ciertas reglas","20/02/2024");
+      expect(practica.fecha).toEqual("20/02/2024");
+  });
 });
