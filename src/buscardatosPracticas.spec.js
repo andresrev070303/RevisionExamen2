783f9f8560afa75a2c6c15a4c23b9753790be647
Isaac71412079 <isaac.rivero@ucb.edu.co>
2024-05-02 10:27:51 -0400

    Verifica si practica tiene los datos correctos

--- src/buscardatosPracticas.spec.js
+++ src/buscardatosPracticas.spec.js
@@ -12,18 +12,26 @@ describe("Practicas System Tests - Search Feature", () => {
       let practica1 = new Practicas();
       practica1.cargarDatos("FizzBuzz", "Some description", "2024-01-01", "http://example.com");
       practicas.push(practica1);
-      
+      let practica2 = new Practicas();
+      practica2.cargarDatos("Totalizador", "Another description", "2024-02-02", "http://example2.com");
+      practicas.push(practica2);
     });
   
-    it("Deberia encontrar una practica por su nombre como el de FizzBuzz", () => {
-      const foundPractica = practicas.find(practica => practica.nombre === "FizzBuzz");
-      expect(foundPractica.nombre).toEqual("FizzBuzz");
-    });
+        it("Deberia encontrar una practica por su nombre como el de FizzBuzz", () => {
+            const foundPractica = practicas.find(practica => practica.nombre === "FizzBuzz");
+            expect(foundPractica.nombre).toEqual("FizzBuzz");
+        });
+    
+      it("NO encuentra practica si no la encuentra", () => {
+        const foundPractica = practicas.find(practica => practica.nombre === "NonExistent");
+        expect(foundPractica).toBeUndefined();
+      });
   
-    it("should not find a practice if the name does not match", () => {
-      const foundPractica = practicas.find(practica => practica.nombre === "NonExistent");
-      expect(foundPractica).toBeUndefined();
+    it("Verifica si practica tiene los datos correctos", () => {
+      const foundPractica = practicas.find(practica => practica.nombre === "Totalizador");
+      expect(foundPractica.descripcion).toEqual("Another description");
+      expect(foundPractica.fecha).toEqual("2024-02-02");
+      expect(foundPractica.enlace).toEqual("http://example2.com");
     });
-  
   
   });
