4578677107255646c682d62e531cc8b905d1a33e
Isaac71412079 <isaac.rivero@ucb.edu.co>
2024-05-02 10:22:27 -0400

    NO encuentra practica si no la encuentra

--- src/buscardatosPracticas.spec.js
+++ src/buscardatosPracticas.spec.js
@@ -12,11 +12,18 @@ describe("Practicas System Tests - Search Feature", () => {
       let practica1 = new Practicas();
       practica1.cargarDatos("FizzBuzz", "Some description", "2024-01-01", "http://example.com");
       practicas.push(practica1);
+      
     });
   
     it("Deberia encontrar una practica por su nombre como el de FizzBuzz", () => {
       const foundPractica = practicas.find(practica => practica.nombre === "FizzBuzz");
       expect(foundPractica.nombre).toEqual("FizzBuzz");
     });
+  
+    it("should not find a practice if the name does not match", () => {
+      const foundPractica = practicas.find(practica => practica.nombre === "NonExistent");
+      expect(foundPractica).toBeUndefined();
+    });
+  
   
   });
