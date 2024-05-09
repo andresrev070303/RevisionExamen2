68f935a92d7b2c47b5ddd626d57b51b3f8727aac
Andres Revollo <andres.revollo@usb.edu.bo>
2024-05-08 23:59:06 -0400

    Se agrego la funcionalidad de encontrar un proyecto cuando existen varios proyectos

--- src/buscar.spec.js
+++ src/buscar.spec.js
@@ -10,4 +10,11 @@ describe('buscador', () => {
         proyectos.push("ejercicio1");
         expect(buscarProyecto("ejercicio1", proyectos)).toEqual("ejercicio1");
     });
+    it("Encuentra un proyecto cuando el mismo existe en una lista de varios proyectos", () => {
+        let proyectos = [];
+        proyectos.push("ejercicio1");
+        proyectos.push("ejercicio2");
+        proyectos.push("ejercicio3");
+        expect(buscarProyecto("ejercicio2", proyectos)).toEqual("ejercicio2");
+    });
 });
